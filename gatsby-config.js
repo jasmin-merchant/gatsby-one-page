04963399f5fb9ba98e7bec880861c3e72bebc3bd
Jasmin Merchant <jasmin@Jasmins-MacBook-Air.local>
2020-04-01 17:25:27 +0530

    Fixed issue of fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         ],
         display: 'swap'
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
